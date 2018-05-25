3e565dfcc6a993f3c3198c2b7fa2e9405dd27dac
Hanninenjp <jphanninen@gmail.com>
2018-05-25 14:35:46 +0300

    Added and configured Cors middleware.

--- ContactsWebApi/ContactsWebApi/Startup.cs
+++ ContactsWebApi/ContactsWebApi/Startup.cs
@@ -33,6 +33,10 @@ namespace ContactsWebApi
             {
                 options.UseSqlServer(Configuration["LocalDbConnection"]);
             });
+
+            services.AddCors(options => options.AddPolicy("AllowAnyPolicy",
+                builder => { builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader(); }));
+
             services.AddMvc();
         }
 
@@ -43,6 +47,8 @@ namespace ContactsWebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors("AllowAnyPolicy");
 
             app.UseMvc();
         }
